5f021cf7132e17c3c49d96afc801f23d25d9f6a9
baha_abk@hotmail.com <BAboukoura@spartaglobal.com>
2019-09-09 13:59:49 +0100

    first scenario passes

--- src/main/java/pageObjectModels/HomePage.java
+++ src/main/java/pageObjectModels/HomePage.java
@@ -1,6 +1,16 @@
 package pageObjectModels;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class HomePage {
 
+    public static WebElement womenCategory(WebDriver driver){
+        return driver.findElement(By.className("sf-with-ul"));
+    }
 
+    public static WebElement subCategory(WebDriver driver){
+        return driver.findElement(By.className("navigation_page"));
+    }
 }
--- src/test/java/stepDefinitionFiles/CategoryStepDefinition.java
+++ src/test/java/stepDefinitionFiles/CategoryStepDefinition.java
@@ -3,21 +3,32 @@ package stepDefinitionFiles;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import pageObjectModels.HomePage;
+import testData.DataSet;
+import utilities.DriverUtilities;
 
 public class CategoryStepDefinition {
 
+    WebDriver driver;
+
+    public CategoryStepDefinition(BaseStepDefinition baseStepDefinition){
+        this.driver = baseStepDefinition.driver;
+    }
+
     @Given("the user on home page")
     public void the_user_on_home_page() {
-
+        driver.get(DataSet.homeURL);
     }
 
     @When("the user clicks on Women tab")
     public void the_user_clicks_on_Women_tab() {
-
+        HomePage.womenCategory(driver).click();
     }
 
     @Then("redirect them to women category")
     public void redirect_them_to_women_category() {
-
+        Assert.assertEquals("Women", HomePage.subCategory(driver).getText());
     }
 }
