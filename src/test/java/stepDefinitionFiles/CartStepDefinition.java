9f191c153b4f0d67555ac11bb8ad64a6512a12bb
DanielJerome <jeromedjp@gmail.com>
2019-09-10 09:00:53 +0100

    clean unecessary code

--- src/test/java/stepDefinitionFiles/CartStepDefinition.java
+++ src/test/java/stepDefinitionFiles/CartStepDefinition.java
@@ -23,21 +23,17 @@ public class CartStepDefinition {
 
     @Given("a user is on the home page")
     public void a_user_is_on_the_home_page() {
-        // Write code here that turns the phrase above into concrete actions
         driver.get(DataSet.homeURL);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @When("a user clicks on the shopping cart")
     public void a_user_clicks_on_the_shopping_cart() {
-        // Write code here that turns the phrase above into concrete actions
         CartPage.CartLink(driver).click();
     }
 
     @Then("the user should be taken to the shopping cart page")
     public void the_user_should_be_taken_to_the_shopping_cart_page() {
-        // Write code here that turns the phrase above into concrete actions
-//        throw new cucumber.api.PendingException();
         String actualCartTitle = CartPage.CartTitle(driver).getText();
         String expectedCartTitle = "SHOPPING-CART SUMMARY";
 
@@ -46,33 +42,24 @@ public class CartStepDefinition {
 
     @Given("user is on a product page")
     public void user_is_on_a_product_page() {
-        // Write code here that turns the phrase above into concrete actions
         driver.get("http://automationpractice.com/index.php?id_category=5&controller=category");
 
     }
 
     @When("the user clicks {string} button")
     public void the_user_clicks_button(String string) {
-        // Write code here that turns the phrase above into concrete actions
         Actions actions = new Actions(driver);
         TShirtPage.TShirtAddToCartButton(driver).click();
-//        actions.moveToElement(TShirtPage.TShirtAddToCartButton(driver)).perform();
     }
 
     @Then("the user should see that product within their shopping cart")
     public void the_user_should_see_that_product_within_their_shopping_cart() {
-        // Write code here that turns the phrase above into concrete actions
-//        throw new cucumber.api.PendingException();
         TShirtPage.ProceedToCheckoutButton(driver).click();
 
         String expectedCartTitle = "SHOPPING-CART SUMMARY";
         CartPage.CartTitle(driver).getText().contains(expectedCartTitle);
 
-
-//        Assert.assertEquals(expectedCartTitle, actualCartTitle);
-
         String expectedProductName = "Faded Short Sleeve T-shirts";
-//        String actualProductName = driver.findElement(By.cssSelector("a[href='http://automationpractice.com/index.php?id_product=1&controller=product#/size-s/color-orange']")).getCssValue();
         String  actualProductName = driver.findElement(By.xpath("//*[@id=\"product_1_1_0_0\"]/td[2]/p/a")).getText();
         Assert.assertEquals(expectedProductName, actualProductName);
 
@@ -80,9 +67,7 @@ public class CartStepDefinition {
 
     @Given("user is viewing the shopping cart page")
     public void user_is_viewing_the_shopping_cart_page() {
-        // Write code here that turns the phrase above into concrete actions
         driver.get("http://automationpractice.com/index.php?id_category=5&controller=category");
-        Actions actions = new Actions(driver);
         TShirtPage.TShirtAddToCartButton(driver).click();
         TShirtPage.ProceedToCheckoutButton(driver).click();
         String expectedCartTitle = "SHOPPING-CART SUMMARY";
@@ -94,20 +79,15 @@ public class CartStepDefinition {
 
     @When("the user clicks the {string} button on a product to add another item to the basket")
     public void the_user_clicks_the_button_on_a_product_to_add_another_item_to_the_basket(String string) {
-        // Write code here that turns the phrase above into concrete actions
         CartPage.AddItemButton(driver).click();
         driver.navigate().refresh();
     }
 
     @Then("the total number of products within the basket should increase")
     public void the_total_number_of_products_within_the_basket_should_increase() {
-        // Write code here that turns the phrase above into concrete actions
-//        throw new cucumber.api.PendingException();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         String value = CartPage.QuantityWindow(driver).getAttribute("value");
         Assert.assertEquals("2", value);
-//        System.out.println(value);
-
     }
 
 }
