50f4fe08c3069b92c17ce69f52f4d70868e14360
Taoheed Afolayan <TAfolayan@spartaglobal.com>
2019-09-10 16:31:32 +0100

    Step Definition test stubs created

---
+++ src/test/java/stepDefinitionFiles/PaymentStepDefinition.java
@@ -0,0 +1,92 @@
+package stepDefinitionFiles;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import pageObjectModels.AddressPaymentPage;
+import testData.DataSet;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PaymentStepDefinition {
+
+    private WebDriver driver = null;
+
+public PaymentStepDefinition(BaseStepDefinition baseStepDefinition) {
+    this.driver = baseStepDefinition.driver;
+}
+
+    @Given("that I navigate to the register page")
+    public void that_I_navigate_to_the_register_page() {
+        driver.get(DataSet.authenticationURL);
+    }
+
+    @Given("I register an account")
+    public void i_register_an_account() {
+        RegistrationObjectModel.registration(driver);
+    }
+
+    @Given("I log in with the account details")
+    public void i_log_in_with_the_account_details() {
+        // Write code here that turns the phrase above into concrete actions
+//        LoginPage.emailAddress(driver).sendkeys("something");
+        driver.findElement(By.id("email")).sendKeys("");
+        driver.findElement(By.id("passwd")).sendKeys("");
+        driver.findElement(By.id("SubmitLogin")).submit();
+//        LoginPage.password(driver).sendkeys("something");
+//        LoginPage.signIn(driver).submit();
+    }
+
+    @When("I add an item to the basket")
+    public void i_add_an_item_to_the_basket() {
+        driver.get(DataSet.tShirtCategoryURL);
+        TShirtPage.TShirtAddToCartButton(driver).click();
+    }
+
+    @When("I click to proceed to payment")
+    public void i_click_to_proceed_to_payment() {
+        CartPage.ProceedToCheckout.click();
+    }
+
+    @Then("I should be taken to the address page")
+    public void i_should_be_taken_to_the_address_page() {
+        assertThat(AddressPaymentPage.addressDropdown(driver).isDisplayed());
+    }
+
+    @Given("that I am on the address page")
+    public void that_I_am_on_the_address_page() {
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I go to the shipping page")
+    public void i_go_to_the_shipping_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("the terms of service radio button has not been clicked")
+    public void the_terms_of_service_radio_button_has_not_been_clicked() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("I should see an alert when I try to continue")
+    public void i_should_see_an_alert_when_I_try_to_continue() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("the terms of service button has been clicked")
+    public void the_terms_of_service_button_has_been_clicked() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("I should be directed to the next page")
+    public void i_should_be_directed_to_the_next_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+}
--- src/test/java/testData/DataSet.java
+++ src/test/java/testData/DataSet.java
@@ -3,4 +3,6 @@ package testData;
 public class DataSet {
 
     public static String homeURL = "http://automationpractice.com/index.php";
+    public static String authenticationURL = "http://automationpractice.com/index.php?controller=authentication&back=history";
+
 }
