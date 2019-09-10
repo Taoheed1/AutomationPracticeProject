01354f31ecb7ef7f60d62fa86474243320cd464d
Taoheed Afolayan <TAfolayan@spartaglobal.com>
2019-09-10 10:35:53 +0100

    Page object models created

---
+++ src/test/java/pageObjectModels/ShippingPaymentPage.java
@@ -0,0 +1,30 @@
+package pageObjectModels;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class ShippingPaymentPage {
+
+    private static WebElement element = null;
+
+    public static WebElement termsAndConditionsCheckbox(WebDriver driver) {
+        element = driver.findElement(By.id("cgv"));
+        return element;
+    }
+
+    public static WebElement termsAndConditionsAlertWindow(WebDriver driver) {
+        element = driver.findElement(By.className("fancybox-error"));
+        return element;
+    }
+
+    public static WebElement deliveryOptionPrice(WebDriver driver) {
+        element = driver.findElement(By.className("delivery_option_price"));
+        return element;
+    }
+
+    public static WebElement termsAndConditions(WebDriver driver) {
+        element = driver.findElement(By.className("iframe"));
+        return element;
+    }
+}
