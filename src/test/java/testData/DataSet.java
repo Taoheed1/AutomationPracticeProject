4550d5bb020010b5b58df6de8208509799a38a8c
DanielJerome <jeromedjp@gmail.com>
2019-09-10 17:17:31 +0100

    remove target files and edit DataSet

--- src/test/java/testData/DataSet.java
+++ src/test/java/testData/DataSet.java
@@ -4,5 +4,5 @@ public class DataSet {
 
     public static String homeURL = "http://automationpractice.com/index.php";
     public static String tShirtCategoryURL = homeURL + "?id_category=5&controller=category";
-    public static String dressesURL = "http://automationpractice.com/index.php?id_category=8&controller=category";
+    public static String dressesURL = homeURL + "?id_category=8&controller=category";
 }
